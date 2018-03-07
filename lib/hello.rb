bec12b8af25cb5bdac571d7f56cd9491e4fce13f
Tim Wheeler <tim (at) timwheeler.com>
2018-03-07 03:47:18 +0000

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,10 @@
 # Default is "World"
 #Author: Tim Wheeler (tim@timwheeler.com)
 
+require 'greeter'
+
 puts "What's your name"
 my_name = gets.strip
 
-puts "Hello, #{my_name}!"
+greeter = Greeter.new(my_name)
+puts greeter.greet
